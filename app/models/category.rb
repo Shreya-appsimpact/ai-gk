6d879ad0a1494d7295b9de2fa15819d8e5ec22d2
shreya-appsimpact <shreya.appsimpacttech@gmail.com>
2019-11-25 19:11:39 +0530

    design issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
-  has_many :questions
+  has_many :questions, dependent: :destroy
+
+  validates :name, uniqueness: true
 end
