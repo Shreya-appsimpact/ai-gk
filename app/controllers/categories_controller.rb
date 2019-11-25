12d7a150710f250bd7e08b4735574f5f8b76c4ec
shreya-appsimpact <shreya.appsimpacttech@gmail.com>
2019-11-25 11:44:56 +0530

    added link on books

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,6 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-
     respond_to do |format|
       if @category.save
         format.html { redirect_to @category, notice: 'Category was successfully created.' }
