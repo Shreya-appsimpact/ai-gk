582cbd6092119cea43ebfe25b8db080357579714
shreya-appsimpact <shreya.appsimpacttech@gmail.com>
2019-12-17 19:17:09 +0530

    association on models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_many :questions, dependent: :destroy
+  has_many :videos 
 
   validates :name, uniqueness: true
 end
