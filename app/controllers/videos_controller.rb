186ee8c58022aff499f3c51d28b62b2547d501d8
shreya-appsimpact <shreya.appsimpacttech@gmail.com>
2019-12-18 14:33:34 +0530

    category video page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,12 @@
 class VideosController < ApplicationController
 
-  def index
-    @videos = Video.all
+  def index 
+    if params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @videos = @category.videos 
+    else
+      @videos = Video.all 
+    end   
   end
 
   def show
